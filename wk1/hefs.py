268b853761efdda3ab54aa7a5c82ac1f5106f11d
Ding, Changhao <1792568382@qq.com>
2022-10-10 09:02:24 +0100

    new

--- wk1/hefs.py
+++ wk1/hefs.py
@@ -4,4 +4,10 @@ def win(name):
     for line in pl:
         pl_name = line.split()[0]
         if pl_name == name:
-            pl.seek
+            newfile = open("new.txt", "w")
+            for l in pl:
+                pl_name = l.split()[0]
+                if pl_name != name:
+                    newfile.write(l)
+                else:
+                    newl = pl_name + " " + str(int(l.split()[0]) + 1)
