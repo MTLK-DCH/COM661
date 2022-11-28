97617b5ebbb19a6624379f1295a5e8a6d165b860
Ding, Changhao <1792568382@qq.com>
2022-11-28 12:49:08 +0000

    d1

--- wk9_D1D2/D1/src/app/app.module.ts
+++ wk9_D1D2/D1/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BusinessesComponent } from './businesses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, BusinessesComponent
   ],
   imports: [
     BrowserModule,
